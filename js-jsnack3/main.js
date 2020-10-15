0286db338e5930c9e2fa3832a6a98d4e0f5f164b
Alberto <thebertsinfo@gmail.com>
2020-10-15 13:56:51 +0200

    jsnack3

---
+++ js-jsnack3/main.js
@@ -0,0 +1,12 @@
+//Il software deve chiedere per 5 volte all’utente di inserire un
+//numero. Il programma stampa la somma di tutti i numeri
+//inseriti.
+var numero1 = parseInt(prompt('Inserisci un numero'));
+var numero2 = parseInt(prompt('Inserisci un numero'));
+var numero3 = parseInt(prompt('Inserisci un numero'));
+var numero4 = parseInt(prompt('Inserisci un numero'));
+var numero5 = parseInt(prompt('Inserisci un numero'));
+
+var somma = numero1 + numero2 + numero3 + numero4 + numero5;
+
+console.log(somma);
